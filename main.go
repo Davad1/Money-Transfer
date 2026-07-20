721f1b75241d559eca437bc5411343053333b522
Davad1 <davidgrosu689@gmail.com>
2026-07-21 01:59:17 +0200

    fix3

--- main.go
+++ main.go
@@ -80,6 +80,6 @@ func main() {
 
 	// Подсказка
 	fmt.Println("Итого")
-	fmt.Printf("User 1 Баланс: %v", user1.Balance)
-	fmt.Printf("User 2 Баланс: %v", user2.Balance)
+	fmt.Printf("User 1 Баланс: %.2f\n", user1.Balance)
+	fmt.Printf("User 2 Баланс: %.2f\n", user2.Balance)
 }
